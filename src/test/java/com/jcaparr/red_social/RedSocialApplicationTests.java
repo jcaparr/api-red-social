7d5323df81ccc7e7a8b76b2d19abdb525e2414cb
jcaparr <juancaparr0s@hotmail.com>
2025-04-14 23:44:01 -0300

    first commit

---
+++ src/test/java/com/jcaparr/red_social/RedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaparr.red_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
